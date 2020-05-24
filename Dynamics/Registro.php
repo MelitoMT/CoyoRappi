cbd13cb3e236bf9bf1c57926dd2c720f0f5881ae
MelitoMT <marianatellez2003@comunidad.unam.mx>
2020-05-23 20:55:45 -0500

    FIX:links

--- Dynamics/Registro.php
+++ Dynamics/Registro.php
@@ -1,10 +1,12 @@
 <?php
+    /* Si ya especifico qué cliente es llega a este condicional */
     if(isset($_POST['registro']) && $_POST['registro']!='' ){
+        /* Si no es alumno es trbajador o profe/funcionario */
         if(isset($_POST['tipo']) && $_POST['tipo']!='alumno'){
             if(isset($_POST['tipo']) && $_POST['tipo']!='trabajador'){
                 /* INSERTE REGISTRO DE PROFE Y FUNCIONARIO */
                 echo"profe/funcionario<br>";
-                echo"<form method='POST' action='CoyoRappi.php'>
+                echo"<form method='POST' action='CoyoRappi.html'>
                 <input type='text' value=''>Nombre<br>
                 <input type='text' value=''>Colegio<br>
                 <input type='password' value=''>RFC<br>
@@ -12,10 +14,11 @@
                 <input type='submit' name='enviar' value='Siguiente'><br>
                 </form>";
             }
+            /* Si no es profe es trabajador */
             else{
                 /* INSERTE REGISTRO DE TRABAJADOR */
                 echo"trabajador<br>";
-                echo"<form method='POST' action='CoyoRappi.php'>
+                echo"<form method='POST' action='CoyoRappi.html'>
                 <input type='text' value=''>Nombre<br>
                 <input type='password' value=''>No. de Trabajador<br>
                 <input type='password' value=''>contraseña<br>
@@ -23,10 +26,11 @@
 
             }
         }
+        /* Es alumno */
         else{
             /* INSERTE REGISTRO DE ALUMNOS */
             echo"alumno<br>";
-            echo"<form method='POST' action='CoyoRappi.php'>
+            echo"<form method='POST' action='CoyoRappi.html'>
             <input type='text' value=''>Nombre<br>
             <input type='text' value=''>Grupo<br>
             <input type='password' value=''>No. cuenta<br>
@@ -34,6 +38,7 @@
             <input type='submit' name='enviar' value='Siguiente'><br>";
         }
     }
+    /* Si no, aparece este formulario */
     else{
     echo"<form action='Registro.php' method='POST'>
         Elija una opción:<br>
