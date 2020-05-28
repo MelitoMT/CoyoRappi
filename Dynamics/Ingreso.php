84836b19697c183e0ee496b9648b1d425a2b3dc8
MelitoMT <marianatellez2003@comunidad.unam.mx>
2020-05-28 00:26:59 -0500

    FEAT:User guide added(ayuda). Links from homepage navbar fixed. Navbar added to Ingreso, Registro and Ayuda. Admin and Sup log in fixed and separated from userslog in. Design added to Ingreso, Registro and Help. User menu fixes and dir.

--- Dynamics/Ingreso.php
+++ Dynamics/Ingreso.php
@@ -1,113 +1,167 @@
 <?php
-echo '
-<form action="Ingreso.php" method="POST">
-    Ingrese tipo de Usuario:
-    <select name="tipo" required/>
-      <option value="Alumno"> Alumno </option>
-      <option value="Profesor"> Profesor </option>
-      <option value="Funcionario"> Funcionario </option>
-      <option value="Trabajador"> Trabajador </option>
-      <option value="Administrador"> Administrador </option>
-      <option value="Supervisor"> Supervisor </option>
-    </select>
-    <input type="submit" value="Iniciar Sesion">
-</form>
-';
-/*
- Se ppodría alida$a=[];
-$b=[];
-$c=[];
+  echo '<link rel="stylesheet" href="../Style/rappi.css">';   
+  echo'<ul>
+      <li class="nav"><a class="nav" href="../Templates/CoyoRappi.html">Inicio</a></li>
+      <li class="nav"><a class="nav" href="Registro.php">Registrate</a></li>
+      <li class="nav"><a class="nav" href="Ingreso.php">Ingresa</a></li>
+      <li class="nav"><a class="nav" href="#">Ayuda</a></li>
+  </ul>';
+  echo"<br><br><br>";
+  echo"<fieldset>";
+  echo'<img src="../Media/LogoCoyo.png" height="200px">';
+	if(isset($_POST['adm'])){
+		echo '<form action="#" method="POST">
+		<label>Usuario</label>
+		<br>
+		<br>
+		<input name="usuario" type="text">
+		<br>
+		<br>
+		<label> Contraseña</label>
+		<br>
+		<br>
+		<input type="password" name="contrasenia">
+		<br>
+		<br>
+		<input type="submit" value="Ingresar" name="Ingresar" class="submit">
+		</form>';
+	}
+	else{	
+		if(isset($_POST['sup'])){
+			echo '<form action="#" method="POST">
+			<label>Usuario</label>
+			<br>
+			<br>
+			<input name="usuario" type="text">
+			<br>
+			<br>
+			<label> Contraseña</label>
+			<br>
+			<br>
+			<input type="password" name="contrasenia">
+			<br>
+			<br>
+			<input type="submit" value="Ingresar" name="Ingresar" class="submit">
+			</form>';
+		}
+		else{
+			/* Si ya ingresó tipo */
+			if (isset($_POST['tipo']) && $_POST['tipo']!=""){
+			$tipo= $_POST['tipo'];
+				if ($tipo =='Alumno'){
+				echo '<form action= "Pedidos.php"  method="POST">
+				<label> Ingrese su numero de cuenta</label>
+				<br>
+				<br>
+				<input type="number" name="nc"  placeholder="Escribe tu numero de cuenta"  pattern= "[0-9]{9}" title="Tiene que tener 9 dígitos." value="" "^\d{10}">
+				<br>
+				<br>
+				<label> Contraseña</label>
+				<br>
+				<br>
+				<input type="password" name="contrasenia" placeholder="Contraseña">
+				<br>
+				<br>
+				<input type="submit" value="Ingresar" name="Ingresar" class="submit">
+				</form>';
+				}
+				elseif ($tipo =='Trabajador'){
+				echo '<form action="Pedidos.php" method="POST">
+				<label> Ingrese su numero de trabajador</label>
+				<br>
+				<br>
+				<input name="nt" type=number  pattern="[0-9]{9}">
+				<br>
+				<br>
+				<label> Contraseña</label>
+				<br>
+				<br>
+				<input type="password" name="contrasenia" placeholder="Contraseña" >
+				<br>
+				<br>
+				<input type="submit" value="Ingresar" name="Ingresar" class="submit">
+				</form>';
+				}
+				elseif ($tipo =='Profesor'||'Funcionario') {
+				echo '<form action=Pedidos.php" method="POST">
+				<label> Ingrese su RFC</label>
+				<br>
+				<br>
+				<input type=text pattern="^[A-Z]{4}[0-9]{2}((0)[0-9]|((1)[0-2]))(([0-2][0-9]|(3)[0-1]))\w{3}" placeholder="RFC">
+				<br>
+				<br>
+				<label> Contraseña</label>
+				<br>
+				<br>
+				<input type="password" name="contrasenia" placeholder="Contraseña">
+				<br>
+				<br>
+				<input type="submit" value="Ingresar" name="Ingresar" class="submit">
+				</form>';
+				}
+				/*if ($tipo=='Administrador') {
+				echo '
+					<form action="Ingreso.php" method="POST">
+					<label> Usuario </label>
+					<input type=number>
+					Contraseña:
+					<input type="password" name="contrasenia" placeholder="Mínimo 10 caracteres" minlenght="10"/>
+					<input type="submit" value="Ingresar">
+				';
+				if ($tipo=='Supervisor') {
+					echo '
+						<form action="Ingreso.php" method="POST">
+						<label> Usuario </label>
+						<input type=number>
+						Contraseña:
+						<input type="password" name="contrasenia" placeholder="Mínimo 10 caracteres" minlenght="10"/>
+						<input type="submit" value="Ingresar">
+					';
+				}*/
 
-$n=$_POST['name'];
-$p=$_POST['lnf'];
-$m=$_POST['lnm'];
-
-
-$N=strtoupper($n);
-$P=strtoupper($p);
-$M=strtoupper($m);
-
-echo "$N $P $M";
-
-$subN= substr($N,0, 1);
-$subP= substr($P,0, 2);
-$subM= substr($M,0, 1);
-
-echo "<br>";
-echo "$subP$subM$subN";
-
-*/
-
-      if (isset($_POST['tipo']) && $_POST['tipo']!="")
-      {
-        $tipo= $_POST['tipo'];
-        echo $tipo."<br>";
-
-          if ($tipo =='Alumno')
-          {
-            echo '
-                <form action= "Pedidos.php"  method="POST">
-                <label> Ingrese su numero de cuenta</label>
-                 <input type="number" name="nc"  placeholder="Escribe tu numero de cuenta"  pattern= "[0-9]{9}" title="Tiene que tener 9 dígitos." value="" > "^\d{10}"
-               <br>
-                <label> Contrasenia</label>
-                <input type="password" name="contrasenia" placeholder="Mínimo 10 caracteres" minlenght="10"/>
-
-                <input type="submit" value="Hacer Pedido">
-                </form>
-            ';
-          }
-
-          elseif ($tipo =='Trabajador')
-          {
-            echo '
-                <form action="Pedidos.php" method="POST">
-                <label> Ingrese su numero de trabajador</label>
-                <input name="nt" type=number  pattern="[0-9]{9}">
-                <br>
-                <label> Contrasenia</label>
-                <input type="password" name="contrasenia" placeholder="Mínimo 10 caracteres" minlenght="10"/>
-                <input type="submit" value="Hacer Pedido">
-                </form>
-             ';
-          }
-
-
-
-          elseif ($tipo =='Profesor'||'Funcionario') {
-          echo '
-                <form action=Pedidos.php" method="POST">
-                <label> Ingrese su numero RFC</label>
-                <input type=text pattern="^[A-Z]{4}[0-9]{2}((0)[0-9]|((1)[0-2]))(([0-2][0-9]|(3)[0-1]))\w{3}">
-                <br>
-                <label> Contrasenia</label>
-                <input type="password" name="contrasenia" placeholder="Mínimo 10 caracteres" minlenght="10"/>
-                <input type="submit" value="Hacer Pedido">
-                </form>
-            ';
-          }
-
-        }
-
-          /*if ($tipo=='Administrador') {
-            echo '
-                <form action="Ingreso.php" method="POST">
-                <label> Usuario </label>
-                <input type=number>
-                Contraseña:
-                <input type="password" name="contrasenia" placeholder="Mínimo 10 caracteres" minlenght="10"/>
-                <input type="submit" value="Ingresar">
-            ';
-            if ($tipo=='Supervisor') {
-              echo '
-                  <form action="Ingreso.php" method="POST">
-                  <label> Usuario </label>
-                  <input type=number>
-                  Contraseña:
-                  <input type="password" name="contrasenia" placeholder="Mínimo 10 caracteres" minlenght="10"/>
-                  <input type="submit" value="Ingresar">
-              ';
-          }*/
-
-
+			}
+			else{
+			/* Es primera vez */
+			echo'<form action="Ingreso.php" method="POST">
+				Ingrese tipo de Usuario:
+				<br><br>
+				<select name="tipo" required/>
+					<option value="Alumno"> Alumno </option>
+					<option value="Profesor"> Profesor </option>
+					<option value="Funcionario"> Funcionario </option>
+					<option value="Trabajador"> Trabajador </option>
+				</select>
+				<br>
+				<br>
+				<input type="submit" name="cliente" value="Aceptar" class="submit">
+			</form>';
+				/*
+				Se ppodría alida$a=[];
+				$b=[];
+				$c=[];
+			
+				$n=$_POST['name'];
+				$p=$_POST['lnf'];
+				$m=$_POST['lnm'];
+			
+			
+				$N=strtoupper($n);
+				$P=strtoupper($p);
+				$M=strtoupper($m);
+			
+				echo "$N $P $M";
+			
+				$subN= substr($N,0, 1);
+				$subP= substr($P,0, 2);
+				$subM= substr($M,0, 1);
+			
+				echo "<br>";
+				echo "$subP$subM$subN";
+			
+				*/
+			}
+	
+		}
+	}	
+	echo"</fieldset>";	
  ?>
--- Dynamics/Registro.php
+++ Dynamics/Registro.php
@@ -1,53 +1,91 @@
 <?php
-    /* Si ya especifico qué cliente es llega a este condicional */
-    if(isset($_POST['registro']) && $_POST['registro']!='' ){
-        /* Si no es alumno es trbajador o profe/funcionario */
-        if(isset($_POST['tipo']) && $_POST['tipo']!='alumno'){
-            if(isset($_POST['tipo']) && $_POST['tipo']!='trabajador'){
-                /* INSERTE REGISTRO DE PROFE Y FUNCIONARIO */
-                echo"profe/funcionario<br>";
-                echo"<form method='POST' action='CoyoRappi.html'>
-                <input type='text' value=''>Nombre<br>
-                <input type='text' value=''>Colegio<br>
-                <input type='password' value=''>RFC<br>
-                <input type='password' value=''>Contraseña<br>
-                <input type='submit' name='enviar' value='Siguiente'><br>
-                </form>";
+    echo '<link rel="stylesheet" href="../Style/rappi.css">';   
+    echo'<ul>
+        <li class="nav"><a class="nav" href="../Templates/CoyoRappi.html">Inicio</a></li>
+        <li class="nav"><a class="nav" href="Registro.php">Registrate</a></li>
+        <li class="nav"><a class="nav" href="Ingreso.php">Ingresa</a></li>
+        <li class="nav"><a class="nav" href="#">Ayuda</a></li>
+    </ul>';
+    echo"<br><br><br>";
+    echo"<fieldset>";
+    echo'<img src="../Media/LogoCoyo.png" height="200px">';
+    echo"<br>";
+        /* Si ya especifico qué cliente es llega a este condicional */
+        if(isset($_POST['tipo']) && $_POST['tipo']!='' ){
+            /* Si no es alumno es trbajador o profe/funcionario */
+            if(isset($_POST['tipo']) && $_POST['tipo']!='alumno'){
+                if(isset($_POST['tipo']) && $_POST['tipo']!='trabajador'){
+                    /* INSERTE REGISTRO DE PROFE Y FUNCIONARIO */
+                    echo"<form method='POST' action='../Templates/CoyoRappi.html'>
+                    <label>Nombre<label>
+                    <br>          
+                    <input type='text' value='' name='Nombre'><br>
+                    <label>Colegio<label>
+                    <br>  
+                    <input type='text' value='' name='Colegio'><br>
+                    <label>RFC<label>
+                    <br>  
+                    <input type='password' value='' name='RFC'><br>
+                    <label>Contraseña<label>
+                    <br>  
+                    <input type='password' value='' name='Contraseña'><br>
+                    <label>Confirmar contraseña<label>
+                    <br>  
+                    <input type='password' value='' name='Contraseña2'><br><br>
+                    <input type='submit' name='enviar' value='Siguiente' class='submit'><br>
+                    </form>";
+                }
+                /* Si no es profe es trabajador */
+                else{
+                    /* INSERTE REGISTRO DE TRABAJADOR */
+                    echo"<form method='POST' action='../Templates/CoyoRappi.html'>
+                    <label>Nombre<label>
+                    <br> 
+                    <input type='text' value=''>
+                    <br>
+                    <label>No. de Trabajador<label>
+                    <br> 
+                    <input type='password' value=''>
+                    <br>
+                    <label>Contraseña<label>
+                    <br>  
+                    <input type='password' value='' name='Contraseña'>
+                    <br>
+                    <label>Confirmar contraseña<label>
+                    <br>  
+                    <input type='password' value='' name='Contraseña2'><br><br>
+                    <input type='submit' name='enviar' value='Siguiente'><br>";
+
+                }
             }
-            /* Si no es profe es trabajador */
+            /* Es alumno */
             else{
-                /* INSERTE REGISTRO DE TRABAJADOR */
-                echo"trabajador<br>";
-                echo"<form method='POST' action='CoyoRappi.html'>
+                /* INSERTE REGISTRO DE ALUMNOS */
+                echo"alumno<br>";
+                echo"<form method='POST' action='../Templates/CoyoRappi.html'>
                 <input type='text' value=''>Nombre<br>
-                <input type='password' value=''>No. de Trabajador<br>
+                <input type='text' value=''>Grupo<br>
+                <input type='password' value=''>No. cuenta<br>
                 <input type='password' value=''>contraseña<br>
                 <input type='submit' name='enviar' value='Siguiente'><br>";
-
             }
         }
-        /* Es alumno */
+        /* Si no, aparece este formulario */
         else{
-            /* INSERTE REGISTRO DE ALUMNOS */
-            echo"alumno<br>";
-            echo"<form method='POST' action='CoyoRappi.html'>
-            <input type='text' value=''>Nombre<br>
-            <input type='text' value=''>Grupo<br>
-            <input type='password' value=''>No. cuenta<br>
-            <input type='password' value=''>contraseña<br>
-            <input type='submit' name='enviar' value='Siguiente'><br>";
-        }
-    }
-    /* Si no, aparece este formulario */
-    else{
-    echo"<form action='Registro.php' method='POST'>
-        Elija una opción:<br>
-        <input type='radio' name='tipo' value='alumno' required>Alumno<br>
-        <input type='radio' name='tipo' value='profesor' required>Profesor<br>
-        <input type='radio' name='tipo' value='funcionario' required>Funcionario<br>
-        <input type='radio' name='tipo' value='trabajador' required>Trabajador<br>
-        <input type='hidden' name='registro' value='registro'>
-        <input type='submit' name='enviar' value='Siguiente'>
-    </form>";
+        echo"Ingrese tipo de usuario:";
+        echo"<br>";
+        echo"<br>";    
+        echo"<form action='Registro.php' method='POST'>
+            <select name='tipo' required>
+                <option value='alumno'> Alumno </option>
+                <option value='profesor'> Profesor </option>
+                <option value='funcionario'> Funcionario </option>
+                <option value='trabajador'> Trabajador </option>
+            </select>
+            <br>
+            <br>
+            <input type='submit' name='Aceptar' value='Aceptar' class='submit'>  
+        </form>";
+    echo"</fieldset>";
     }
 ?>
